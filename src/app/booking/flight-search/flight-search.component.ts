0ada0e963eb8bb253f35d0af4cbd1872a5ed2e73
Manfred Steyer <manfred.steyer@gmx.net>
2023-02-19 21:31:11 +0100

    ngrx

--- src/app/booking/flight-search/flight-search.component.ts
+++ src/app/booking/flight-search/flight-search.component.ts
@@ -1,16 +1,14 @@
-import { AsyncPipe, JsonPipe, NgForOf, NgIf } from "@angular/common";
-import { Component, inject, OnInit } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { CityValidator } from "@demo/shared";
-import { FlightCardComponent } from "../flight-card/flight-card.component";
-import { ActivatedRoute } from "@angular/router";
-import { signal } from "src/app/signals";
-import { effect } from "src/app/signals/effect";
-import { addMinutes } from "src/app/date-utils";
-import { fromStore } from "src/app/utils";
-import { selectFlights } from "../+state/selectors";
-import { Store } from "@ngrx/store";
-import { loadFlights } from "../+state/actions";
+import { AsyncPipe, JsonPipe, NgForOf, NgIf } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CityValidator } from '@demo/shared';
+import { FlightCardComponent } from '../flight-card/flight-card.component';
+import { ActivatedRoute } from '@angular/router';
+import { signal } from 'src/app/signals';
+import { fromStore } from 'src/app/utils';
+import { selectFlights } from '../+state/selectors';
+import { Store } from '@ngrx/store';
+import { delayFlight, loadFlights } from '../+state/actions';
 
 type ComponentState = {
   from: string;
@@ -25,9 +23,9 @@ const initState: ComponentState = {
   urgent: false,
   basket: {
     3: true,
-    5: true
-  }
-}
+    5: true,
+  },
+};
 
 @Component({
   standalone: true,
@@ -36,30 +34,31 @@ const initState: ComponentState = {
     NgForOf,
     AsyncPipe,
     JsonPipe,
-    FormsModule, 
+    FormsModule,
     FlightCardComponent,
     CityValidator,
   ],
   selector: 'flight-search',
-  templateUrl: './flight-search.component.html'
+  templateUrl: './flight-search.component.html',
 })
 export class FlightSearchComponent implements OnInit {
-
   private route = inject(ActivatedRoute);
 
   store = inject(Store);
-  flights = fromStore(selectFlights)
+
+  // App State
+  flights = fromStore(selectFlights);
+  
+  // Component State
   state = signal(initState);
 
   constructor() {
-
-    this.route.paramMap.subscribe(p => {
+    this.route.paramMap.subscribe((p) => {
       const from = p.get('from');
       const to = p.get('to');
 
       if (from && to) {
-       
-        this.state.update(v => ({
+        this.state.update((v) => ({
           ...v,
           from,
           to,
@@ -70,34 +69,36 @@ export class FlightSearchComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   // Helper function for data binding
   update(key: string, event: any): void {
-    this.state.update(v => ({
+    this.state.update((v) => ({
       ...v,
-      [key]: event.target.value
+      [key]: event.target.value,
     }));
   }
 
   // Helper function for data binding
   updateCheckbox(key: string, event: any): void {
-    this.state.update(v => ({
+    this.state.update((v) => ({
       ...v,
-      [key]: event.target.checked
+      [key]: event.target.checked,
     }));
   }
 
   search(): void {
     if (!this.state().from || !this.state().to) return;
 
-    this.store.dispatch(loadFlights(this.state().from, this.state().to));
+    this.store.dispatch(
+      loadFlights({ from: this.state().from, to: this.state().to })
+    );
   }
 
   // Just delay the first flight
   delay(): void {
-    
+    const flights = this.flights();
+    const id = flights[0].id;
+    this.store.dispatch(delayFlight({ id }));
   }
 }
-
