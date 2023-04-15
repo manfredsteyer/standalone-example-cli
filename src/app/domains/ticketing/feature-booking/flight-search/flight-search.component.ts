a4a12697be3df1f144cb31c0ac53a73411088014
Manfred Steyer <manfred.steyer@gmx.net>
2023-04-15 20:04:32 +0200

    introduced signals

--- src/app/domains/ticketing/feature-booking/flight-search/flight-search.component.ts
+++ src/app/domains/ticketing/feature-booking/flight-search/flight-search.component.ts
@@ -1,5 +1,5 @@
 import {AsyncPipe, JsonPipe, NgForOf, NgIf} from "@angular/common";
-import {Component, inject} from "@angular/core";
+import {Component, WritableSignal, inject, signal} from "@angular/core";
 import {FormsModule} from "@angular/forms";
 import {Store} from "@ngrx/store";
 import {take} from "rxjs";
@@ -7,6 +7,7 @@ import {ActivatedRoute} from "@angular/router";
 import {BookingSlice, Flight, FlightService, delayFlight, loadFlights, selectFlights} from "../../data";
 import {CityValidator} from "src/app/shared/util-common";
 import {FlightCardComponent} from "../../ui-common";
+import { ChangeDetectionStrategy } from "@angular/core";
 
 @Component({
   standalone: true,
@@ -22,23 +23,24 @@ import {FlightCardComponent} from "../../ui-common";
     CityValidator,
   ],
   selector: 'app-flight-search',
-  templateUrl: './flight-search.component.html'
+  templateUrl: './flight-search.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FlightSearchComponent  {
 
   private flightService = inject(FlightService);
   private route = inject(ActivatedRoute);
 
-  from = 'Hamburg'; // in Germany
-  to = 'Graz'; // in Austria
-  urgent = false;
+  from = signal('Hamburg'); // in Germany
+  to = signal('Graz'); // in Austria
+  urgent = signal(false);
 
-  flights: Flight[] = [];
+  flights: WritableSignal<Flight[]> = signal([]);
 
-  basket: Record<number, boolean> = {
+  basket: WritableSignal<Record<number, boolean>> = signal({
     3: true,
     5: true
-  };
+  });
 
   constructor() {
     this.route.paramMap.subscribe(p => {
@@ -46,8 +48,8 @@ export class FlightSearchComponent  {
       const to = p.get('to');
 
       if (from && to) {
-        this.from = from;
-        this.to = to;
+        this.from.set(from);
+        this.to.set(to);
         this.search();
       }
     });
@@ -56,21 +58,28 @@ export class FlightSearchComponent  {
   search(): void {
     if (!this.from || !this.to) return;
 
-    this.flightService.find(this.from, this.to).subscribe(
+    this.flightService.find(this.from(), this.to()).subscribe(
       flights => {
-        this.flights = flights;
+        this.flights.set(flights);
       }
     );
 
   }
 
   delay(): void {
-    const date = new Date(this.flights[0].date);
+    const flights = this.flights();
+    const date = new Date(flights[0].date);
     date.setTime(date.getTime() + 1000 * 60 * 15);
 
     // Mutable
-    this.flights[0].date = date.toISOString();
-
+    // this.flights.mutate(flights => {
+    //   flights[0].date = date.toISOString()
+    // });
+    
+    this.flights.update(flights => ([
+      { ...flights[0], date: date.toISOString() },
+      ...flights.slice(1)
+    ]))
     // Immutable
     // const newFlight: Flight = { ...this.flights[0], date: date.toISOString()}
     // const newFlights = [newFlight, ...this.flights.slice(1)];
