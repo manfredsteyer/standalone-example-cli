d1aebff000ff57e0916dad4dfa40fa41feb584a0
Manfred Steyer <manfred.steyer@gmx.net>
2023-04-18 16:15:39 +0200

    done

--- src/app/domains/ticketing/feature-booking/flight-search/flight-search.component.ts
+++ src/app/domains/ticketing/feature-booking/flight-search/flight-search.component.ts
@@ -1,12 +1,13 @@
 import {AsyncPipe, JsonPipe, NgForOf, NgIf} from "@angular/common";
-import {Component, inject} from "@angular/core";
+import {ChangeDetectionStrategy, Component, computed, effect, inject, signal} from "@angular/core";
 import {FormsModule} from "@angular/forms";
 import {Store} from "@ngrx/store";
-import {take} from "rxjs";
+import {combineLatest, debounceTime, filter, interval, startWith, take, takeUntil} from "rxjs";
 import {ActivatedRoute} from "@angular/router";
 import {BookingSlice, Flight, FlightService, delayFlight, loadFlights, selectFlights} from "../../data";
 import {CityValidator} from "src/app/shared/util-common";
 import {FlightCardComponent} from "../../ui-common";
+import { toSignal, toObservable, takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   standalone: true,
@@ -22,37 +23,90 @@ import {FlightCardComponent} from "../../ui-common";
     CityValidator,
   ],
   selector: 'app-flight-search',
-  templateUrl: './flight-search.component.html'
+  templateUrl: './flight-search.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FlightSearchComponent  {
 
+  private store = inject(Store);
   private flightService = inject(FlightService);
 
-  from = 'Hamburg'; // in Germany
-  to = 'Graz'; // in Austria
-  urgent = false;
+  // Signal <|--- WritableSignal
 
-  flights: Flight[] = [];
+  from = signal('Hamburg'); // in Germany
+  to = signal('Graz'); // in Austria
+  
+  from$ = toObservable(this.from);
+  to$ = toObservable(this.to);
 
-  basket: Record<number, boolean> = {
+  criteria$ = combineLatest({
+    from: this.from$, 
+    to: this.to$,
+    dummy: interval(1000),
+  })
+    .pipe(
+      filter(combi => combi.from.length >= 3 && combi.to.length >= 3),
+      debounceTime(300),
+      // startWith
+  );
+
+  criteria = toSignal(this.criteria$, 
+    { initialValue: { from: 'Hamburg', to: ' Graz', dummy: 0} });
+  
+  // criteria2 = toSignal(this.criteria$, { requireSync: true })
+
+  flightRoute = computed(() => this.from() + ' to ' + this.to());
+  
+  urgent = signal(false);
+
+  flights = toSignal(this.store.select(selectFlights), { requireSync: true });
+  
+  // NGRX 16: this.store.selectSignal(selectFlights)
+  
+  // flights = signal<Flight[]>([]);
+
+  constructor() {
+
+    // setTimeout(() => {
+    //   this.from.set('London');
+    //   // Glitch: London - Graz
+    //   this.to.set('Paris');
+    //   // London - Paris
+    // }, 2000);
+
+    effect(() => {
+      // console.log(this.flightRoute());
+      this.search();
+      //console.log(this.criteria().dummy);
+    });
+
+    this.criteria$.pipe(takeUntilDestroyed()).subscribe(x => console.log('criteria$', x));
+
+
+  }
+
+  basket = signal<Record<number, boolean>>({
     3: true,
     5: true
-  };
+  });
 
-  search(): void {
-    if (!this.from || !this.to) return;
+  async search() {
 
-    this.flightService.find(this.from, this.to).subscribe(
-      flights => {
-        this.flights = flights;
-      }
-    );
+    const c = this.criteria();
+
+    if (!c.from || !c.to) return;
+
+    this.store.dispatch(loadFlights({ from: c.from, to: c.to }));
+   
   }
 
   delay(): void {
-    const date = new Date(this.flights[0].date);
-    date.setTime(date.getTime() + 1000 * 60 * 15);
-    this.flights[0].date = date.toISOString();
+    const flights = this.flights();
+    const id = flights[0].id;
+
+    this.store.dispatch(delayFlight({id}));
+
+
   }
 
 }
