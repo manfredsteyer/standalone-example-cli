e6ff3669bbd44fc3bcf498ba0d7375ab82f362af
Manfred Steyer <manfred.steyer@gmx.net>
2023-04-29 15:01:34 +0200

    prepare feature showcase

--- src/app/domains/ticketing/feature-booking/flight-search/flight-search.component.ts
+++ src/app/domains/ticketing/feature-booking/flight-search/flight-search.component.ts
@@ -1,29 +1,14 @@
 import { AsyncPipe, JsonPipe, NgForOf, NgIf } from '@angular/common';
 import { Component, computed, effect, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Store } from '@ngrx/store';
 import {
-  combineLatest,
-  debounceTime,
-  filter,
-  interval,
-  startWith,
-  take,
-  tap,
-} from 'rxjs';
-import { ActivatedRoute } from '@angular/router';
-import {
-  BookingSlice,
   Flight,
   FlightService,
-  delayFlight,
-  loadFlights,
-  selectFlights,
 } from '../../data';
 import { CityValidator } from 'src/app/shared/util-common';
 import { FlightCardComponent } from '../../ui-common';
 import { ChangeDetectionStrategy } from '@angular/core';
-import { toObservable, toSignal, takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { addMinutes } from 'src/app/shared/util-common/date-utils';
 
 @Component({
   standalone: true,
@@ -42,76 +27,35 @@ import { toObservable, toSignal, takeUntilDestroyed } from '@angular/core/rxjs-i
 })
 export class FlightSearchComponent {
   private flightService = inject(FlightService);
-  private store = inject(Store);
-
-  // Signal <|--- WritableSignal
 
   from = signal('Hamburg'); // in Germany
   to = signal('Graz'); // in Austria
-
-  from$ = toObservable(this.from);
-  to$ = toObservable(this.to);
-
-  criteria$ = combineLatest({
-    from: this.from$,
-    to: this.to$,
-    dummy: interval(1000)
-  }).pipe(
-    filter((combi) => combi.from.length >= 3 && combi.to.length >= 3),
-    debounceTime(300),
-  );
-
-  // criteria = toSignal(this.criteria$, {
-  //   initialValue: {
-  //     from: 'Graz',
-  //     to: 'Hamburg',
-  //     dummy: -1
-  //   },
-  // });
-
   flightRoute = computed(() => this.from() + ' to ' + this.to());
 
   urgent = signal(false);
-
-  flights = toSignal(this.store.select(selectFlights), {
-    requireSync: true,
-  });
-
+  flights = signal<Flight[]>([]);
   basket = signal<Record<number, boolean>>({
     3: true,
     5: true,
   });
 
   constructor() {
-
-    this.criteria$.pipe(takeUntilDestroyed()).subscribe(x => console.log(x));
-
-    effect(() => {
-      console.log('route', this.flightRoute());
-    });
-
-    setTimeout(() => {
-      this.from.set('Paris');
-      // Glitch: Paris - Graz   -- Push/Pull
-      // Glitch-free!
-      this.to.set('London');
-    }, 2000);
-
     effect(() => {
       this.search();
     });
-
   }
 
   async search() {
-  //   const c = this.criteria();
-  //   if (!c.from || !c.to) return;
-  //   this.store.dispatch(loadFlights({ from: c.from, to: c.to }));
+    if (!this.from() || !this.to()) return;
+    const flights = await this.flightService.findPromise(this.from(), this.to());
+    this.flights.set(flights);
   }
 
   delay(): void {
     const flights = this.flights();
-    const id = flights[0].id;
-    this.store.dispatch(delayFlight({ id }));
+    const flight = flights[0];
+    this.flights.mutate(flights => {
+      flights[0].date = addMinutes(flight.date, 15);
+    });
   }
 }
---
+++ src/app/shared/util-common/date-utils.ts
@@ -0,0 +1,6 @@
+export function addMinutes(dateAsStr: string, minutes: number): string {
+    const date = new Date(dateAsStr);
+    date.setTime(date.getTime() + minutes * 60 * 1000);
+    return date.toISOString();
+  }
+  
