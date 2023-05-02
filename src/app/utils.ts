47e49e6fb3dcd2c0542159e7d7dde10364398b8d
Manfred Steyer <manfred.steyer@gmx.net>
2023-05-02 23:51:19 +0200

    experiment

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -53,3 +53,46 @@ export function state<T extends Object>(obj: T): T {
   }
 }
 
+
+export type DeepSignal<Type> = {
+  [Property in keyof Type]: Type[Property] extends object
+    ? WritableSignal<DeepSignal<Type[Property]>>
+    : WritableSignal<Type[Property]>;
+};
+
+export function nest<T>(value: T): WritableSignal<DeepSignal<T>> {
+  const signalMap = new Map<string | symbol, WritableSignal<unknown>>();
+
+  if (typeof value === 'object') {
+    const deep = {} as DeepSignal<T>;
+
+    for (const key of Object.keys(value as object)) {
+      Object.defineProperty(deep, key, {
+        get() {
+            return getSignal(key, value);
+        },
+        set(v: unknown) {
+            const s = getSignal(key, value);
+            s?.set(v);
+            (value as any)[key] = v;
+        },
+      });
+    }
+    return signal(deep);
+  } else {
+    return signal(value as DeepSignal<T>);
+  }
+
+  function getSignal(
+    prop: string | symbol,
+    target: T
+  ): WritableSignal<unknown> | undefined {
+    if (!signalMap.has(prop)) {
+      const value = (target as any)[prop];
+      const s = signal<unknown>(value);
+      signalMap.set(prop, s);
+    }
+    const s = signalMap.get(prop);
+    return s;
+  }
+}
