259f843c08d48811ec2e7e983ade443337d9fa9a
Manfred Steyer <manfred.steyer@gmx.net>
2023-05-06 14:11:02 +0200

    blink in flight-search

--- src/app/shell/sidebar/sidebar.component.ts
+++ src/app/shell/sidebar/sidebar.component.ts
@@ -1,6 +1,6 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, inject } from '@angular/core';
 import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatLegacyListModule as MatListModule } from '@angular/material/legacy-list';
@@ -8,6 +8,9 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 import { map, shareReplay } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { selectFlights } from 'src/app/booking/+state/selectors';
+import { delayFlight } from 'src/app/booking/+state/actions';
 
 @Component({
     standalone: true,
@@ -25,6 +28,10 @@ import { map, shareReplay } from 'rxjs';
     styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+
+    store = inject(Store);
+    flights = this.store.selectSignal(selectFlights);
+    
     isHandset$ = this.breakpointObserver.observe(Breakpoints.Handset)
         .pipe(
             map(result => result.matches),
@@ -33,5 +40,11 @@ export class SidebarComponent {
 
     constructor(
         @Inject(BreakpointObserver) private breakpointObserver: BreakpointObserver) {
+    }
+
+    delay(): void {
+        const flights = this.flights();
+        const id = flights[0].id;
+        this.store.dispatch(delayFlight({ id }));
     }
 }
