827dd3b4c035e57f310eeeed55a277482cb6f7d0
Manfred Steyer <manfred.steyer@gmx.net>
2023-05-15 22:11:14 +0200

    done

--- src/app/domains/ticketing/data/flight-booking.facade.ts
+++ src/app/domains/ticketing/data/flight-booking.facade.ts
@@ -1,37 +1,55 @@
 import { Injectable, inject } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { ticketingFeature } from './+state/reducers';
-import { ticketingActions } from './+state/actions';
+import { ComponentStore } from '@ngrx/component-store';
+import { Flight } from './flight';
+import { FlightService } from './flight.service';
+import { addMinutes } from 'src/app/shared/util-common';
 
 @Injectable({ providedIn: 'root' })
 export class FlightBookingFacade {
-  private store = inject(Store);
 
-  criteria = this.store.selectSignal(ticketingFeature.selectCriteria);
-  basket = this.store.selectSignal(ticketingFeature.selectBasket);
-  flights = this.store.selectSignal(ticketingFeature.selectFlights);
+  private flightService = inject(FlightService);
+
+  private store = new ComponentStore({
+    flights: [] as Flight[],
+    criteria: {
+      from: 'Graz',
+      to: 'Hamburg',
+    },
+    basket: {} as Record<number, boolean>,
+  });
+
+  criteria = this.store.selectSignal((s) => s.criteria);
+  basket = this.store.selectSignal((s) => s.basket);
+  flights = this.store.selectSignal((s) => s.flights);
 
   updateCriteria(from: string, to: string): void {
-    this.store.dispatch(ticketingActions.updateCriteria({ from, to }));
+    this.store.patchState({ criteria: { from, to } });
   }
 
   updateBasket(id: number, selected: boolean): void {
-    this.store.dispatch(ticketingActions.updateBasket({ id, selected }));
+    this.store.patchState((s) => ({ basket: { ...s.basket, [id]: selected } }));
   }
 
   async load() {
-    if (!this.criteria().from || !this.criteria().to) return;
-    this.store.dispatch(
-      ticketingActions.loadFlights({
-        from: this.criteria().from,
-        to: this.criteria().to,
-      })
-    );
+    const from = this.criteria().from;
+    const to = this.criteria().to;
+
+    if (!from || !to) return;
+   
+    const flights = await this.flightService.findPromise(from, to);
+    this.store.patchState({ flights });
   }
 
   delay(): void {
     const flights = this.flights();
-    const id = flights[0].id;
-    this.store.dispatch(ticketingActions.delayFlight({ id }));
+    const flight = flights[0];
+    
+    const updFlight: Flight = { ...flight, date: addMinutes(flight.date, 15)};
+    const updFlights: Flight[] = [
+      updFlight,
+      ...flights.slice(1)
+    ];
+
+    this.store.patchState({ flights: updFlights });
   }
 }
