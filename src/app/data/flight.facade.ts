a96d7c17ca6591297befcd6c0a1bfff16929ac81
Manfred Steyer <manfred.steyer@gmx.net>
2023-10-13 20:36:18 +0200

    add signalState

--- src/app/data/flight.facade.ts
+++ src/app/data/flight.facade.ts
@@ -1,13 +1,16 @@
-import { Injectable, computed, inject, signal } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { Flight } from './flight';
 import { FlightService } from './flight.service';
 import { lastValueFrom } from 'rxjs';
+import { signalState } from '../shared/signal-store/signal-state';
+import { selectSignal } from '../shared/signal-store/select-signal';
+import { patchState } from '../shared/signal-store/patch-state';
 
 @Injectable({ providedIn: 'root' })
 export class FlightFacade {
   private flightService = inject(FlightService);
 
-  private state = signal({
+  private state = signalState({
     from: 'Graz',
     to: 'Hamburg',
     urgent: false,
@@ -15,14 +18,16 @@ export class FlightFacade {
     basket: {} as Record<number, boolean>,
   });
 
-  readonly from = computed(() => this.state().from);
-  readonly to = computed(() => this.state().to);
-  readonly urgent = computed(() => this.state().urgent);
-  readonly flights = computed(() => this.state().flights);
-  readonly basket = computed(() => this.state().basket);
+  readonly from = this.state.from;
+  readonly to = this.state.to;
+  readonly urgent = this.state.urgent;
+  readonly flights = this.state.flights;
+  readonly basket = this.state.basket;
 
-  readonly selected = computed(() =>
-    this.state().flights.filter((f) => this.state().basket[f.id])
+  readonly selected = selectSignal(
+    this.flights,
+    this.basket,
+    (flights, basket) => flights.filter((f) => basket[f.id])
   );
 
   async load(): Promise<void> {
@@ -30,29 +35,19 @@ export class FlightFacade {
       this.flightService.find(this.from(), this.to(), this.urgent())
     );
 
-    this.state.update((state) => ({
-      ...state,
-      flights,
-    }));
+    patchState(this.state, { flights });
   }
 
   updateCriteria(criteria: { from?: string; to?: string }): void {
-    this.state.update((state) => ({
-      ...state,
-      from: criteria.from || state.from,
-      to: criteria.to || state.to,
-    }));
+    patchState(this.state, { from: criteria.from, to: criteria.to });
   }
 
   updateBasket(flightId: number, selected: boolean): void {
-    this.state.update((state) => ({
-      ...state,
+    patchState(this.state, (state) => ({
       basket: {
         ...state.basket,
         [flightId]: selected,
       },
     }));
   }
-
-
 }
