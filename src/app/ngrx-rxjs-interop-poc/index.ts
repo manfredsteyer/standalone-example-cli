676d5d78b089e3489d7b354e04fa960f522be56f
Manfred Steyer <manfred.steyer@gmx.net>
2023-11-22 19:37:26 +0100

    prepare for ng 17

--- src/app/ngrx-rxjs-interop-poc/index.ts
+++
@@ -1,1 +0,0 @@
-export { rxMethod } from './rx-method';
--- src/app/ngrx-signal-store-poc/index.ts
+++
@@ -1,8 +0,0 @@
-export { PartialStateUpdater, patchState } from './patch-state';
-export { signalState } from './signal-state';
-export { signalStore } from './signal-store';
-export { signalStoreFeature, type } from './signal-store-feature';
-export { withComputed } from './with-computed';
-export { withHooks } from './with-hooks';
-export { withMethods } from './with-methods';
-export { withState } from './with-state';
