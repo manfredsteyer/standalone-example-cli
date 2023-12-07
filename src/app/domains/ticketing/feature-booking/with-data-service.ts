773f2589afa56ab4aa8f47d91b2a8b8b06492f52
Manfred Steyer <manfred.steyer@gmx.net>
2023-12-07 13:47:09 +0100

    done

---
+++ src/app/domains/ticketing/feature-booking/with-data-service.ts
@@ -0,0 +1,76 @@
+import { signalStoreFeature } from "@ngrx/signals";
+import { patchState, withComputed, withHooks, withMethods, withState } from "@ngrx/signals";
+import { Flight, FlightService } from "../data";
+import { computed, inject } from "@angular/core";
+import { Criteria } from "./criteria";
+import { addMinutes } from "src/app/shared/util-common";
+import { rxMethod } from '@ngrx/signals/rxjs-interop';
+import { debounceTime, delay, filter, switchMap, tap } from "rxjs";
+import { setLoaded, setLoading, withCallState } from "./call-state";
+
+export function withDataService() {
+
+    return signalStoreFeature(
+        withState({
+            from: 'Hamburg',
+            to: 'London',
+            flights: [] as Flight[],
+            basket: {} as Record<number, boolean>
+        }),
+        withComputed((store) => ({
+            selected: computed(() => store.flights().filter(f => store.basket()[f.id])),
+            criteria: computed(() => ({ from: store.from(), to: store.to() }))
+        })),
+        withCallState(),
+        withMethods((
+            store,
+            flightService = inject(FlightService)
+        ) => ({
+            async load(): Promise<void> {
+                const flights = await flightService.findPromise(store.from(), store.to());
+                patchState(store, { flights });
+            },
+            updateCriteria(c: Criteria): void {
+                patchState(store, c);
+            },
+            updateBasket(flightId: number, selected: boolean): void {
+                patchState(store, ({ basket }) => ({
+                    basket: {
+                        ...basket,
+                        [flightId]: selected
+                    }
+                }));
+            },
+            delay(): void {
+                patchState(store, ({ flights }) => ({
+                    flights: [
+                        {
+                            ...flights[0],
+                            date: addMinutes(flights[0].date, 15)
+                        },
+                        ...flights.slice(1)
+                    ]
+                }));
+            },
+            connectCriteria: rxMethod<Criteria>(c$ => c$.pipe(
+                filter(c => c.from.length >= 3 && c.to.length >= 3),
+                debounceTime(300),
+                tap(() => patchState(store, setLoading())),
+                switchMap(c => flightService.find(c.from, c.to)),
+                delay(7000),
+                tap(flights => patchState(store, { flights })),
+                tap(() => patchState(store, setLoaded())),
+            ))
+        })),
+        withHooks({
+            onInit(store) {
+                // store.load();
+                store.connectCriteria(store.criteria);
+            },
+            onDestroy(store) {
+                console.log('Hasta la vista, Store!', store)
+            }
+        })
+
+    )
+}
