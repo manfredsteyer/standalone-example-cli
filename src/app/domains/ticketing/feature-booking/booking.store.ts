0cf522c18ee613478964e961b2023326a0149d69
Manfred Steyer <manfred.steyer@gmx.net>
2024-01-12 10:53:24 +0100

    done

---
+++ src/app/domains/ticketing/feature-booking/booking.store.ts
@@ -0,0 +1,59 @@
+import { patchState, signalStore, withComputed, withHooks, withMethods, withState } from "@ngrx/signals";
+import { Flight, FlightService } from "../data";
+import { computed, inject } from "@angular/core";
+import { Criteria } from "./criteria";
+import { addMinutes } from "src/app/shared/util-common";
+import { rxMethod } from '@ngrx/signals/rxjs-interop';
+
+export const BookingStore = signalStore(
+    { providedIn: 'root' },
+    withState({
+        from: 'Hamburg',
+        to: 'London',
+        flights: [] as Flight[],
+        basket: {} as Record<number, boolean>
+    }),
+    withComputed((store) => ({
+        selected: computed(() => store.flights().filter(f => store.basket()[f.id])),
+        criteria: computed(() => ({ from: store.from, to: store.to }))
+    })),
+    withMethods((
+        store,
+        flightService = inject(FlightService)
+    ) => ({
+        async load(): Promise<void> {
+            const flights = await flightService.findPromise(store.from(), store.to());
+            patchState(store, { flights });
+        },
+        updateCriteria(c: Criteria): void {
+            patchState(store, c);
+        },
+        updateBasket(flightId: number, selected: boolean): void {
+            patchState(store, ({ basket }) => ({
+                basket: {
+                    ...basket,
+                    [flightId]: selected
+                }
+            }));
+        },
+        delay(): void {
+            patchState(store, ({ flights }) => ({
+                flights: [
+                    {
+                        ...flights[0],
+                        date: addMinutes(flights[0].date, 15)
+                    },
+                    ...flights.slice(1)
+                ]
+            }));
+        }
+    })),
+    withHooks({
+        onInit(store) {
+            store.load();
+        },
+        onDestroy(store) {
+            console.log('Hasta la vista, Store!', store)
+        }
+    })
+)
