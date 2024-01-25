16069dec3362f08951aaba85b5e98c89aea4255c
Manfred Steyer <manfred.steyer@gmx.net>
2024-01-25 21:15:29 +0100

    add nf

---
+++ src/bootstrap.ts
@@ -0,0 +1,39 @@
+import { LayoutModule } from '@angular/cdk/layout';
+import {
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import {
+  PreloadAllModules,
+  provideRouter,
+  withPreloading,
+} from '@angular/router';
+import { AppComponent } from './app/app.component';
+import { APP_ROUTES } from './app/app.routes';
+import { authInterceptor } from './app/shared/util-auth';
+import { provideLogger } from './app/shared/util-logger';
+import { TicketsModule } from './app/domains/ticketing/feature-my-tickets';
+import { loggerConfig } from './app/logger.config';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(
+      withInterceptors([authInterceptor]),
+    ),
+
+    provideRouter(
+      APP_ROUTES,
+      withPreloading(PreloadAllModules)
+    ),
+
+    provideLogger(loggerConfig),
+
+    provideAnimations(),
+
+    importProvidersFrom(TicketsModule),
+    importProvidersFrom(LayoutModule),
+  ],
+});
--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,6 @@
-import { LayoutModule } from '@angular/cdk/layout';
-import {
-  provideHttpClient,
-  withInterceptors,
-} from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import {
-  PreloadAllModules,
-  provideRouter,
-  withPreloading,
-} from '@angular/router';
-import { AppComponent } from './app/app.component';
-import { APP_ROUTES } from './app/app.routes';
-import { authInterceptor } from './app/shared/util-auth';
-import { provideLogger } from './app/shared/util-logger';
-import { TicketsModule } from './app/domains/ticketing/feature-my-tickets';
-import { loggerConfig } from './app/logger.config';
+import { initFederation } from '@angular-architects/native-federation';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient(
-      withInterceptors([authInterceptor]),
-    ),
-
-    provideRouter(
-      APP_ROUTES,
-      withPreloading(PreloadAllModules)
-    ),
-
-    provideLogger(loggerConfig),
-
-    provideAnimations(),
-
-    importProvidersFrom(TicketsModule),
-    importProvidersFrom(LayoutModule),
-  ],
-});
+initFederation()
+  .catch(err => console.error(err))
+  .then(_ => import('./bootstrap'))
+  .catch(err => console.error(err));
