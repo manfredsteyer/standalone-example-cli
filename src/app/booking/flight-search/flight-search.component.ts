bb6c448c235f14cda539df90929f3c73a663ee8f
Manfred Steyer <manfred.steyer@gmx.net>
2024-03-02 16:05:27 +0100

    update example

--- src/app/booking/flight-search/flight-search.component.ts
+++ src/app/booking/flight-search/flight-search.component.ts
@@ -1,10 +1,10 @@
 import { AsyncPipe, JsonPipe, NgForOf, NgIf } from '@angular/common';
-import { Component, computed, effect, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CityValidator } from '@demo/shared';
 import { FlightCardComponent } from '../flight-card/flight-card.component';
-import { Flight, FlightService } from '@demo/data';
-import { combineLatest, debounceTime, filter } from 'rxjs';
+import { FlightService } from '@demo/data';
+import { combineLatest, debounceTime, filter, switchMap } from 'rxjs';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
@@ -21,57 +21,24 @@ import { toObservable, toSignal } from '@angular/core/rxjs-interop';
   selector: 'flight-search',
   templateUrl: './flight-search.component.html',
 })
-export class FlightSearchComponent implements OnInit {
+export class FlightSearchComponent {
   private flightService = inject(FlightService);
 
   from = signal('Hamburg');
   to = signal('Graz');
-  flights = signal<Flight[]>([]);
-
   basket = signal<Record<number, boolean>>({ 1: true });
-  urgent = signal(false);
-
   flightRoute = computed(() => this.from() + ' to ' + this.to());
 
   from$ = toObservable(this.from);
   to$ = toObservable(this.to);
 
-  criteria$ = combineLatest({ from: this.from$, to: this.to$ }).pipe(
-    filter(combi => combi.from.length >= 3 && combi.to.length >= 3),
+  flights$ = combineLatest({ from: this.from$, to: this.to$ }).pipe(
+    filter(c => c.from.length >= 3 && c.to.length >= 3),
     debounceTime(300),
+    switchMap(c => this.flightService.find(c.from, c.to))
   );
 
-  criteria = toSignal(this.criteria$, { 
-    initialValue: {
-      from: '',
-      to: '',
-    }
+  flights = toSignal(this.flights$, {
+    initialValue: []
   });
-
-  // criteria2 = toSignal(this.criteria$, { 
-  //   requireSync: true
-  // });
-
-  constructor() {
-    effect(() => {
-      console.log('route:', this.flightRoute());
-    });
-
-    effect(() => {
-      this.search();
-    });
-  }
-
-  async search() {
-    const {from, to} = this.criteria();
-
-    if (!from || !to) {
-      return;
-    }
-
-    const flights = await this.flightService.findAsPromise(from, to);
-    this.flights.set(flights);
-  }
-
-  ngOnInit(): void {}
 }
