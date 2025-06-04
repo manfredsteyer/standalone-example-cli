8f13649e5ce3366f6818da9c2f74c533b2853c81
Manfred Steyer <manfred.steyer@gmx.net>
2025-06-04 19:37:27 +0200

    update to ng20

--- src/app/shell/about/lazy/lazy.component.ts
+++ src/app/shell/about/lazy/lazy.component.ts
@@ -2,10 +2,9 @@ import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 
 @Component({
-  standalone: true,
-  selector: 'app-lazy',
-  imports: [CommonModule],
-  template: `<h2 *ngIf="visible">{{ title }}</h2>`
+    selector: 'app-lazy',
+    imports: [CommonModule],
+    template: `<h2 *ngIf="visible">{{ title }}</h2>`
 })
 export class LazyComponent {
   title = 'Standalone Demo';
